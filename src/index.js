b6042c9cc78c6c7e61e671df50c8c64040e978f4
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-09-19 16:42:38 +0700

    setting bootstrap and fontawesome

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const person = {
-  name: 'Dimas',
-  walk() {
-    console.log(this.name);
-  }
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
 
-console.log(this.walk);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-person.walk();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
